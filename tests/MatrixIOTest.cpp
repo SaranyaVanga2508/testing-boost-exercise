fa1cdcbba961dbe8740ee857b19b933a26c86b8c
root <root@DESKTOP-IB1IV6N.>
2025-02-04 16:10:04 +0100

    Fix: Applied clang-format to comply with coding standards

--- tests/MatrixIOTest.cpp
+++ tests/MatrixIOTest.cpp
@@ -1,7 +1,7 @@
 #define BOOST_TEST_DYN_LINK
 
-#include <boost/test/unit_test.hpp>
 #include <Eigen/Dense>
+#include <boost/test/unit_test.hpp>
 #include <fstream>
 #include <iostream>
 #include "../src/matrixIO.hpp"
@@ -15,34 +15,33 @@ BOOST_AUTO_TEST_SUITE(MatrixIOTestSuite, *boost::unit_test::tolerance(1e-6))
 // **Test Case: Successfully read a matrix from a CSV file**
 BOOST_AUTO_TEST_CASE(TestOpenData)
 {
-    const string testFileName = "test_matrix.csv"; 
+  const string testFileName = "test_matrix.csv";
 
-    // Create expected 3x3 matrix
-    MatrixXd expectedMatrix(3, 3);
-    expectedMatrix << 0.680375, 0.59688, -0.329554,
-                     -0.211234, 0.823295, 0.536459,
-                      0.566198, -0.604897, -0.444451;
+  // Create expected 3x3 matrix
+  MatrixXd expectedMatrix(3, 3);
+  expectedMatrix << 0.680375, 0.59688, -0.329554,
+      -0.211234, 0.823295, 0.536459,
+      0.566198, -0.604897, -0.444451;
 
-    // Write to a CSV file
-    ofstream file(testFileName);
-    if (file.is_open()) {
-        file << "0.680375, 0.59688, -0.329554\n"
-             << "-0.211234, 0.823295, 0.536459\n"
-             << "0.566198, -0.604897, -0.444451\n";
-        file.close();
-    } else {
-        BOOST_FAIL("Could not create test CSV file!");
-    }
+  // Write to a CSV file
+  ofstream file(testFileName);
+  if (file.is_open()) {
+    file << "0.680375, 0.59688, -0.329554\n"
+         << "-0.211234, 0.823295, 0.536459\n"
+         << "0.566198, -0.604897, -0.444451\n";
+    file.close();
+  } else {
+    BOOST_FAIL("Could not create test CSV file!");
+  }
 
-    // Read matrix from the CSV file
-    MatrixXd loadedMatrix = openData(testFileName, 3);
+  // Read matrix from the CSV file
+  MatrixXd loadedMatrix = openData(testFileName, 3);
 
-    // Compare with expected matrix
-    BOOST_TEST(loadedMatrix.isApprox(expectedMatrix));
+  // Compare with expected matrix
+  BOOST_TEST(loadedMatrix.isApprox(expectedMatrix));
 
-    // **Delete test file after the test**
-    remove(testFileName.c_str());
+  // **Delete test file after the test**
+  remove(testFileName.c_str());
 }
-
 
 BOOST_AUTO_TEST_SUITE_END()
